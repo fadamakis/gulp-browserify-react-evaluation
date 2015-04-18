ae3f157721c94ee37832f08fc3863bf5250594a2
Fotis Adamakis <fotis316@yahoo.com>
2015-04-18 17:37:35 +0300

    Add stylus support

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,6 +3,14 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
+var stylus = require('gulp-stylus');
+
+// Get one .styl file and render 
+gulp.task('stylus', function () {
+  gulp.src('./css/app.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('./css/'));
+});
 
 gulp.task('browserify', function(){
     return browserify('./js/app.js')
@@ -12,6 +20,9 @@ gulp.task('browserify', function(){
     .pipe(gulp.dest('js'))
 });
 
+gulp.task('default', ['stylus','browserify']);
+
 gulp.task('watch', function(){
-    gulp.watch('**/*.js', ['browserify']);
+    gulp.watch('js/*.js', ['browserify']);
+    gulp.watch('css/*.styl', ['stylus']);
 });
